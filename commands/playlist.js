baa9136675c782c5378cc28af520528fceb575e2
EvS444 <emanuelsacoman@gmail.com>
2024-01-06 10:51:11 -0300

    Envio básico

---
+++ commands/playlist.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("playlist")
+        .setDescription("Ouça a melhor playlist de estudos"),
+
+    async execute(interaction) {
+        await interaction.reply("https://open.spotify.com/playlist/3FbsZmt8BrFFmVpKD4ju4H?si=3fb6ffe4339d462e")
+    }
+}
---
+++ deploy-commands.js
@@ -0,0 +1,32 @@
+const { REST, Routes } = require("discord.js");
+const dotenv = require('dotenv');
+dotenv.config();
+const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
+
+const fs = require("node:fs");
+const path = require("node:path");
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+const commands = [];
+
+for (const file of commandFiles){
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: "10" }).setToken(TOKEN);
+
+(async () => {
+    try {
+        console.log(`Resetando ${commands.length} comandos...`);
+
+        // Correção do trecho onde é feita a chamada para enviar os comandos
+        await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands });
+        
+        console.log("Comandos registrados com sucesso!");
+    }
+    catch(error) {
+        console.error(error);
+    }
+})();
