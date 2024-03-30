320c606e7e7eb9b644e56e301f319fa873d380b5
EvS444 <emanuelsacoman@gmail.com>
2024-03-30 11:36:51 -0300

    Adicionado QRCode

---
+++ commands/qrcode.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const qrcode = require("qrcode");
+const fs = require("fs");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("qrcode")
+        .setDescription("Gera um QR code a partir de um link fornecido")
+        .addStringOption(option =>
+            option.setName("link")
+                .setDescription("O link para o qual gerar o QR code")
+                .setRequired(true)),
+
+    async execute(interaction) {
+        const link = interaction.options.getString("link");
+        const qrCodeFileName = `qrcode_${Date.now()}.png`;
+
+        await generateQRCode(link, qrCodeFileName);
+
+        const qrCodeEmbed = new EmbedBuilder()
+            .setColor("#ff9c00")
+            .setTitle("QR Code Gerado")
+            .setDescription(`Aqui está o QR code para o link: ${link}`)
+            .setImage(`attachment://${qrCodeFileName}`)
+            .setFooter({ text: "CodeBucket", iconURL: "https://cdn-icons-png.flaticon.com/512/190/190544.png" })
+            .setTimestamp();
+
+        await interaction.reply({ embeds: [qrCodeEmbed], files: [qrCodeFileName] });
+
+        // Remover o arquivo após a resposta ter sido enviada
+        fs.unlinkSync(qrCodeFileName);
+    },
+};
+
+async function generateQRCode(link, fileName) {
+    try {
+        await qrcode.toFile(fileName, link);
+    } catch (error) {
+        console.error("Erro ao gerar o QR code:", error);
+    }
+}
