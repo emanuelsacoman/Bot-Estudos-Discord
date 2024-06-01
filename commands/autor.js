febb08fbe23b1ead79d6e7681a55b50dbe1eb43d
EvS444 <emanuelsacoman@gmail.com>
2024-06-01 07:57:41 -0300

    Alterado descrição

--- commands/autor.js
+++ commands/autor.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder, EmbedBuilder } = require("discord.js")
 const exampleEmbed = new EmbedBuilder()
 	.setColor('Blue')
 	.setTitle('Quem fez?')
-	.setDescription('**CodeBucket** é um projeto a nível acadêmico, feito por *Emanuel Vinícius Sacoman* (eu) durante o processo de criação do TCC no curso de Análise e Desenvolvimento de Sistemas, que tem como objetivo ajudar usuários a se familiarizarem com a utilização do Discord para estudos voltados para a área de programação básica.')
+	.setDescription('**CodeBucket** é um projeto a nível acadêmico, feito por *Emanuel Vinícius Sacoman* (eu) durante o processo de criação do TCC no curso de Análise e Desenvolvimento de Sistemas, que tem como objetivo ajudar usuários a se familiarizarem com a utilização do Discord para estudos voltados para a área de programação.')
 	.setURL('https://github.com/emanuelsacoman')
 	.setFooter({ text: 'Emanuel', iconURL: 'https://avatars.githubusercontent.com/u/63565495?v=4' })
 	.setTimestamp();
