44f29fb0c19d41ed36d1d0391feb679f19de3784
EvS444 <emanuelsacoman@gmail.com>
2025-04-25 13:04:45 -0300

    Adicionado alguns botões

--- commands/invite.js
+++ commands/invite.js
@@ -1,19 +1,34 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js")
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("invite")
-        .setDescription("Gera um convite do CodeBucket para seu servidor"),
+  data: new SlashCommandBuilder()
+    .setName('invite')
+    .setDescription('Gera um convite do CodeBucket para seu servidor'),
 
-    async execute(interaction){
-        const exampleEmbed = new EmbedBuilder()
-            .setColor('Blue')
-            .setTitle('CLIQUE AQUI')
-            .setDescription('O link enviará você até o convite do CodeBucket.')
-            .setURL('https://discord.com/oauth2/authorize?client_id=1193011045577523300')
-            .setFooter({ text: 'CodeBucket', iconURL: 'https://cdn-icons-png.flaticon.com/512/190/190544.png' })
-            .setTimestamp();
-        const message = await interaction.reply({ embeds: [exampleEmbed], fetchReply: true })
-        await message.react('❤️');
-    }
-}
+  async execute(interaction) {
+    const inviteURL = process.env.INVITE_URL ||
+      'https://discord.com/oauth2/authorize?client_id=1193011045577523300&permissions=0&scope=bot%20applications.commands';
+
+    const exampleEmbed = new EmbedBuilder()
+      .setColor('Blue')
+      .setTitle('📨 Convite do CodeBucket')
+      .setDescription('Clique no botão abaixo para adicionar o CodeBucket ao seu servidor!')
+      .setFooter({ text: 'CodeBucket', iconURL: 'https://cdn-icons-png.flaticon.com/512/190/190544.png' })
+      .setTimestamp();
+
+    const inviteButton = new ButtonBuilder()
+      .setLabel('Adicionar ao servidor')
+      .setStyle(ButtonStyle.Link)
+      .setURL(inviteURL);
+
+    const row = new ActionRowBuilder().addComponents(inviteButton);
+
+    const message = await interaction.reply({
+      embeds: [exampleEmbed],
+      components: [row],
+      fetchReply: true
+    });
+
+    await message.react('❤️');
+  }
+};
--- commands/vote.js
+++ commands/vote.js
@@ -1,19 +1,33 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js")
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("vote")
-        .setDescription("Vote no CodeBucket em top.gg!"),
+  data: new SlashCommandBuilder()
+    .setName('vote')
+    .setDescription('Vote no CodeBucket em top.gg!'),
 
-    async execute(interaction){
-        const exampleEmbed = new EmbedBuilder()
-            .setColor('#911d57')
-            .setTitle("CLIQUE AQUI")
-            .setDescription('Vote no CodeBucket em top.gg!')
-            .setURL('https://top.gg/bot/1193011045577523300')
-            .setFooter({ text: 'CodeBucket', iconURL: 'https://cdn-icons-png.flaticon.com/512/190/190544.png' })
-            .setTimestamp();
-        const message = await interaction.reply({ embeds: [exampleEmbed], fetchReply: true })
-        await message.react('⭐');
-    }
-}
+  async execute(interaction) {
+    const topggURL = 'https://top.gg/bot/1193011045577523300';
+
+    const exampleEmbed = new EmbedBuilder()
+      .setColor('#911d57')
+      .setTitle('⭐ Vote no CodeBucket!')
+      .setDescription('Ajude o CodeBucket a crescer votando em top.gg. Use o botão abaixo para votar agora!')
+      .setFooter({ text: 'CodeBucket', iconURL: 'https://cdn-icons-png.flaticon.com/512/190/190544.png' })
+      .setTimestamp();
+
+    const voteButton = new ButtonBuilder()
+      .setLabel('Votar no top.gg')
+      .setStyle(ButtonStyle.Link)
+      .setURL(topggURL);
+
+    const row = new ActionRowBuilder().addComponents(voteButton);
+
+    const message = await interaction.reply({
+      embeds: [exampleEmbed],
+      components: [row],
+      fetchReply: true
+    });
+
+    await message.react('⭐');
+  }
+};
